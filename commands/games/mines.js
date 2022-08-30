b7d8eec6c3413d94fd1160783dd81f7062b1c956
sopy <doimpt@sopy.one>
2022-08-30 23:03:07 +0300

    Mines: Other fix to invalid inputs

--- commands/games/mines.js
+++ commands/games/mines.js
@@ -30,9 +30,9 @@ module.exports = {
             let x = controls.charCodeAt(0) - 97,
                 y = parseInt(controls.charAt(1)) - 1
 
-            if (x < 1 || y < 1) return;
+            if (x < 0 || y < 0) return;
 
-            if (x > game.board.data?.[0].length || y > game.board.data.length) return;
+            if (x >= game.board.data?.[0].length || y >= game.board.data.length) return;
 
             if (controls.length == 3) {
                 game.flag(x,y)
